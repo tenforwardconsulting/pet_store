e4f96381ec087bfec9bf2dfb0e2de6005b563eeb
Alex Petitjean <alex@tenforwardconsulting.com>
2018-08-15 14:07:56 -0500

    Generate Pet model
    `rails g model pet name:string species:string breed:string store:references`

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_15_185907) do
+ActiveRecord::Schema.define(version: 2018_08_15_190722) do
+
+  create_table "pets", force: :cascade do |t|
+    t.string "name"
+    t.string "species"
+    t.string "breed"
+    t.integer "store_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["store_id"], name: "index_pets_on_store_id"
+  end
 
   create_table "stores", force: :cascade do |t|
     t.string "name"
