c73ec2ec0488c0363fed24f289d678df64b17ea5
Alex Petitjean <alex@tenforwardconsulting.com>
2018-08-15 14:02:22 -0500

    Generate Store model
    `rails g model store name:string description:text`

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_15_174206) do
+ActiveRecord::Schema.define(version: 2018_08_15_185907) do
+
+  create_table "stores", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
